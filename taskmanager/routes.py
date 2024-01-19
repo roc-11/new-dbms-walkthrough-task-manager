95aec329a469aff52cfcf323bc1a506a2de294d5
roc-11 <>
2024-01-19 16:40:13 +0000

    Fix: fix error in add task function

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -53,7 +53,6 @@ def add_task():
             due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
         )
-        category = Category(category_name=request.form.get("category_name"))
         db.session.add(task)
         db.session.commit()
         return redirect(url_for("home"))
